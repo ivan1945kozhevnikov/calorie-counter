daa57442c628852a16d3c27043d73009386535b9
Kozhevnikov Ivan <kozhevnikovivan86@gmail.com>
2023-03-30 09:19:56 +0300

    The method responsible for initializing the counter

--- src/js/modules/counter.js
+++ src/js/modules/counter.js
@@ -1,10 +1,25 @@
 class Counter {
   constructor(element) {
     this.root = element;
+    this.form = this.root.querySelector('.counter__form');
+    this.elements = this.form.elements;
+    this.inputAge = this.elements.age;
+    this.inputHeight = this.elements.height;
+    this.inputWeight = this.elements.weight;
+
+    this._onFieldInput = this._onFieldInput.bind(this);
+  }
+
+  _onFieldInput() {
+    console.log(this.inputAge.value);
+    console.log(this.inputHeight.value);
+    console.log(this.inputWeight.value);
   }
 
   init() {
-    console.log(this.root);
+    this.inputAge.addEventListener('input', this._onFieldInput);
+    this.inputHeight.addEventListener('input', this._onFieldInput);
+    this.inputWeight.addEventListener('input', this._onFieldInput);
   }
 }
 
