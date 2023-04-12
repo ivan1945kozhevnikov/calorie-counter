1837c35cabcbda5e7ab4b3458d8aa2947cb074cc
Kozhevnikov Ivan <kozhevnikovivan86@gmail.com>
2023-04-12 14:21:46 +0300

    Create agroup of constant in a new file

--- src/js/modules/counter.js
+++ src/js/modules/counter.js
@@ -1,12 +1,9 @@
 import formatInput from '../utils/formatInput.js';
+import countResult from '../utils/countResult.js';
 
 class Counter {
   constructor(element) {
     this.root = element;
-    this.spanMin = document.getElementById('calories-minimal');
-    this.spanNorm = document.getElementById('calories-norm');
-    this.spanMax = document.getElementById('calories-maximal');
-
     this.form = this.root.querySelector('.counter__form');
     this.result = this.root.querySelector('.counter__result');
     this.inputsGroup = this.form.querySelector('.inputs-group');
@@ -37,72 +34,12 @@ class Counter {
 
   _onButtonClick() {
     this.submitButton.setAttribute('type', 'button');
-    this.result.classList.remove('counter__result--hidden');
-    const PERCENTAGE = 0.15;
-    const MIN_COEF = 1.2;
-    const LOW_COEF = 1.375;
-    const MEDIUM_COEF = 1.55;
-    const HIGH_COEF = 1.725;
-    const MAX_COEF = 1.9;
-
-    if (this.genderRadios.value === 'male') {
-      const N =
-        10 * this.weightInput.value +
-        6.25 * this.heightInput.value -
-        5 * this.ageInput.value +
-        5;
-      this.spanMin.textContent = N - N * PERCENTAGE;
-      this.spanMax.textContent = N * PERCENTAGE + N;
-      if (this.activityRadios.value === 'min') {
-        const weighMaintenance = N * MIN_COEF;
-        this.spanNorm.textContent = weighMaintenance;
-      }
-      if (this.activityRadios.value === 'low') {
-        const weighMaintenance = N * LOW_COEF;
-        this.spanNorm.textContent = weighMaintenance;
-      }
-      if (this.activityRadios.value === 'medium') {
-        const weighMaintenance = N * MEDIUM_COEF;
-        this.spanNorm.textContent = weighMaintenance;
-      }
-      if (this.activityRadios.value === 'high') {
-        const weighMaintenance = N * HIGH_COEF;
-        this.spanNorm.textContent = weighMaintenance;
-      }
-      if (this.activityRadios.value === 'max') {
-        const weighMaintenance = N * MAX_COEF;
-        this.spanNorm.textContent = weighMaintenance;
-      }
-    }
-    if (this.genderRadios.value === 'female') {
-      const N =
-        10 * this.weightInput.value +
-        6.25 * this.heightInput.value -
-        5 * this.ageInput.value -
-        161;
-      this.spanMin.textContent = N - N * PERCENTAGE;
-      this.spanMax.textContent = N * PERCENTAGE + N;
-      if (this.activityRadios.value === 'min') {
-        const weighMaintenance = N * MIN_COEF;
-        this.spanNorm.textContent = weighMaintenance;
-      }
-      if (this.activityRadios.value === 'low') {
-        const weighMaintenance = N * LOW_COEF;
-        this.spanNorm.textContent = weighMaintenance;
-      }
-      if (this.activityRadios.value === 'medium') {
-        const weighMaintenance = N * MEDIUM_COEF;
-        this.spanNorm.textContent = weighMaintenance;
-      }
-      if (this.activityRadios.value === 'high') {
-        const weighMaintenance = N * HIGH_COEF;
-        this.spanNorm.textContent = weighMaintenance;
-      }
-      if (this.activityRadios.value === 'max') {
-        const weighMaintenance = N * MAX_COEF;
-        this.spanNorm.textContent = weighMaintenance;
-      }
-    }
+    countResult(
+      this.ageInput,
+      this.weightInput,
+      this.heightInput,
+      this.genderRadios
+    );
   }
 
   _onFormReset() {
